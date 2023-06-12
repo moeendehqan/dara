43546b62a16f0aef2fa0c3454ec937ec52df190e
gholami71 <careless.whisper51@yahoo.com>
2023-06-12 14:23:05 +0430

    شروع desk

--- src/page/company.js
+++ src/page/company.js
@@ -3,14 +3,23 @@ import { OnRun } from "../config/OnRun"
 import { getCookie } from "../Function/cookie"
 import { useNavigate } from "react-router-dom"
 import { setCookie } from "../Function/cookie"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { ImExit } from "react-icons/im"
 
 
 const Company = ()=>{
 
     const navigate = useNavigate()
+    const [datadf, setDatadf] = useState([])
+    console.log(datadf)
 
     const cookie = getCookie('phn')
+    const handleExit = ()=>{
+        setCookie('phn', '', 0)
+        navigate('/')
+
+    }
+
     const CheckCookie = () =>{
         if (cookie){
         axios({method:'POST', url:OnRun+'/dara/checkcookie', data:{cookie:cookie}
@@ -31,14 +40,49 @@ const Company = ()=>{
     useEffect(CheckCookie, [])
 
     const getCompany = () =>{
-        axios.post(OnRun+'/dara/getcompany', {cookie:cookie}).then()
+        axios.post(OnRun+'/dara/getcompany', {cookie:cookie}).then(response=>{
+            if (response.data.replay){
+                setDatadf(response.data.df)
+            }
+        
+        })
+    }
+
+    const handletoCompany =(symbol,volume)=>{
+        if(volume>0){
+           navigate('/desk', {state:symbol})
+        }
     }
 
     useEffect(getCompany, [])
 
     return(
-        <div>
-            
+        <div className="company">
+            <p onClick={handleExit}>
+                <ImExit/>
+            </p>
+            <div className="listcompany">
+                {
+                    datadf.map((items)=>{
+                        return(
+                           
+                           <div className={items['تعداد سهام']==0?'eachcompany ziro':'eachcompany'}
+                            onClick={()=>handletoCompany(items.symbol,items['تعداد سهام'])}>
+                                <div className="title">
+                                    <img src={process.env.PUBLIC_URL+'/img/'+items.icon}></img>
+                                    <div className="names">
+                                        <p>{items.name}</p>
+                                        <p>{items.fullname}</p>
+                                    </div>
+                                </div>
+                                <h2>
+                                     تعداد سهام شما:{items['تعداد سهام'].toLocaleString()}
+                                </h2>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
